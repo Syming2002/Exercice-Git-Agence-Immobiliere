1591289f1e523f37ff78674ba08cbac69999d2b9
Syming2002 <simon.bruneau@yahoo.com>
2026-02-13 11:49:59 +0100

    fix style.css

---
+++ stylelint.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import("stylelint").Config} */
+export default {
+  "extends": ["stylelint-config-standard"],
+  "rules": {
+    "declaration-block-no-duplicate-custom-properties": true
+  }
+};
